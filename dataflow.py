19952e34e5519805f52b9b5fe6da1ee502d71326
Gulshan Gill <gulshngill@Gulshans-MacBook-Pro.local>
2020-04-02 21:58:26 +0800

    Added coder,parser,pardo for spread

--- dataflow.py
+++ dataflow.py
@@ -8,75 +8,111 @@ python dataflow.py \
   --temp_location=gs://$BUCKET_NAME/temp
 '''
 import argparse
-import datetime
 import json
 import logging
-
+import time
 import apache_beam as beam
 import apache_beam.transforms.window as window
+from apache_beam.metrics import Metrics
 from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.transforms import trigger
 
 
-class GroupWindowsIntoBatches(beam.PTransform):
-    """A composite transform that groups Pub/Sub messages based on publish
-    time and outputs a list of dictionaries, where each contains one message
-    and its publish timestamp.
-    """
 
-    def __init__(self, window_size):
-        # Convert minutes into seconds.
-        self.window_size = int(window_size * 60)
+class JsonCoder(object):
+    """A JSON coder interpreting each line as a JSON string."""
 
-    def expand(self, pcoll):
-        return (pcoll
-                # Assigns window info to each Pub/Sub message based on its
-                # publish timestamp.
-                | 'Window into Fixed Intervals' >> beam.WindowInto(
-                    window.FixedWindows(self.window_size))
-                | 'Add timestamps to messages' >> (beam.ParDo(AddTimestamps()))
-                | 'Add Dummy Key' >> beam.Map(lambda elem: (None, elem))
-                | 'Groupby' >> beam.GroupByKey()
-                | 'Abandon Dummy Key' >> beam.MapTuple(lambda _, val: val))
+    def encode(self, x):
+        return json.dumps(x).encode('utf-8')
+
+    def decode(self, x):
+        return json.loads(x)
 
 
-class AddTimestamps(beam.DoFn):
 
-    def process(self, element, publish_time=beam.DoFn.TimestampParam):
+class ParseData(beam.DoFn):
+    """Parse data to only include required fields and filter out header record"""
+    def __init__(self):
+        beam.DoFn.__init__(self)
+        self.num_parse_errors = Metrics.counter(self.__class__, 'num_parse_errors')
 
-        yield {
-            'message_body': element.decode('utf-8'),
-            'publish_time': datetime.datetime.utcfromtimestamp(
-                float(publish_time)).strftime("%Y-%m-%d %H:%M:%S.%f"),
-        }
+    def process(self, elem):
+        try:
+
+            if 'channels' not in elem.keys():
+
+                elem["time"] = elem["time"].replace('T', ' ').replace('Z','')
+
+                yield {
+                'product_id': elem["product_id"],
+                'trade_id': elem["trade_id"],
+                'sequence': elem["sequence"],
+                'timestamp': elem["time"],
+                'price': elem["price"],
+                'best_bid': elem["best_bid"],
+                'best_ask': elem["best_ask"]
+                }
+
+                logging.info('Parsed: %s', elem)
+
+        except:
+          # Log and count parse errors
+          self.num_parse_errors.inc()
+          logging.error('Parse error on "%s"', elem)
 
 
-class WriteBatchesToGCS(beam.DoFn):
+class CalculateSpread(beam.DoFn):
+    """Calculate spread amount and %"""
 
-    def __init__(self, output_path):
-        self.output_path = output_path
+    def __init__(self):
+        beam.DoFn.__init__(self)
+        self.spread_errors = Metrics.counter(self.__class__, 'spread_errors')
 
-    def process(self, batch, window=beam.DoFn.WindowParam):
-        """Write one batch per file to a Google Cloud Storage bucket. """
+    def process(self, elem):
+        try:
+            spread_amt = float(elem["best_ask"]) - float(elem["best_bid"])
+            spread_percentage = spread_amt / float(elem["best_ask"]) * 100
 
-        ts_format = '%H:%M'
-        window_start = window.start.to_utc_datetime().strftime(ts_format)
-        window_end = window.end.to_utc_datetime().strftime(ts_format)
-        filename = '-'.join([self.output_path, window_start, window_end])
+            yield {
+            'product_id': elem["product_id"],
+            'trade_id': elem["trade_id"],
+            'sequence': elem["sequence"],
+            'timestamp': elem["timestamp"],
+            'price': elem["price"],
+            'best_bid': elem["best_bid"],
+            'best_ask': elem["best_ask"],
+            'spread_amt': "{:.2f}".format(spread_amt),
+            'spread_percentage': "{:.2f}".format(spread_percentage)
+            }
 
-        with beam.io.gcp.gcsio.GcsIO().open(filename=filename, mode='w') as f:
-            for element in batch:
-                f.write('{}\n'.format(json.dumps(element)).encode('utf-8'))
+            logging.info('Parsed: %s', elem)
+
+        except:
+          # Log and count parse errors
+          self.spread_errors.inc()
+          logging.error('error on "%s" when calculating spread', elem)
+
+
 
 
 def run(input_topic, output_path, window_size=1.0, pipeline_args=None):
     pipeline_options = PipelineOptions(
         pipeline_args, streaming=True, save_main_session=True)
 
-    with beam.Pipeline(options=pipeline_options) as pipeline:
-        (pipeline
-         | 'Read PubSub Messages' >> beam.io.ReadFromPubSub(topic=input_topic)
-         | 'Window into' >> GroupWindowsIntoBatches(window_size)
-         | 'Write to GCS' >> beam.ParDo(WriteBatchesToGCS(output_path)))
+    with beam.Pipeline(options=pipeline_options) as p:
+        (p
+         #| 'Read PubSub Messages' >> beam.io.ReadFromPubSub(topic=input_topic)
+         | 'ReadText' >> beam.io.ReadFromText("data.json", coder=JsonCoder())
+         | 'ParseJSON' >> beam.ParDo(ParseData())
+         | 'CalculateSpread' >> beam.ParDo(CalculateSpread())
+         | 'AddEventTs' >> beam.Map(lambda elem: beam.window.TimestampedValue(elem, time.mktime(
+                    time.strptime(elem['timestamp'], '%Y-%m-%d %H:%M:%S.%f'))))
+         # | 'GlobalWindow' >> beam.WindowInto(
+         #    beam.window.GlobalWindows(),
+         #    trigger=trigger.Repeatedly(trigger.AfterProcessingTime(1 * 60)), #maybe use afterwatermark(early,late) after watermark only triggers when windowcloses
+         #    accumulation_mode=trigger.AccumulationMode.ACCUMULATING)
+         | 'WriteText' >> beam.io.WriteToText("output.csv"))
+         #| 'Write to GCS' >> beam.ParDo(WriteBatchesToGCS(output_path)))
 
 
 if __name__ == '__main__':
@@ -97,5 +133,4 @@ if __name__ == '__main__':
         help='GCS Path of the output file including filename prefix.')
     known_args, pipeline_args = parser.parse_known_args()
 
-    run(known_args.input_topic, known_args.output_path, known_args.window_size,
-        pipeline_args)
+    run(known_args.input_topic, known_args.output_path, known_args.window_size,pipeline_args)
--- publisher.py
+++ publisher.py
@@ -5,6 +5,7 @@ import ssl
 import argparse
 import logging
 import sys
+import time
 from google.cloud import pubsub
 
 TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
@@ -16,18 +17,20 @@ try:
     import thread
 except ImportError:
     import _thread as thread
-import time
+
 
 
 def on_message(ws, message):
     logging.info('Publishing {}'.format(message))
-    publisher.publish(event_type, message.encode("utf-8")) #data must be encoded in bytestring
+    print(message)
+    #publisher.publish(event_type, message.encode("utf-8")) #data must be encoded in bytestring
+
 
 def on_error(ws, error):
     print(error)
 
 def on_close(ws):
-    print("### closed ###")
+    print("### stream closed ###")
 
 def on_open(ws):
     #send subscription params to server
@@ -43,25 +46,25 @@ def on_open(ws):
 
 if __name__ == "__main__":
     #parse arguments from cmd line
-    parser = argparse.ArgumentParser(description='Send data to Cloud Pub/Sub')
-    parser.add_argument('--project', help='Example: --project $DEVSHELL_PROJECT_ID', required=True)
-    parser.add_argument('--topic', help='Example: --topic crypto', required=True)
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='Send data to Cloud Pub/Sub')
+    # parser.add_argument('--project', help='Example: --project $DEVSHELL_PROJECT_ID', required=True)
+    # parser.add_argument('--topic', help='Example: --topic crypto', required=True)
+    # args = parser.parse_args()
 
 
     # create Pub/Sub notification topic
-    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-    publisher = pubsub.PublisherClient()
-    event_type = publisher.topic_path(args.project,args.topic)
-    print(event_type)
+    # logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+    # publisher = pubsub.PublisherClient()
+    # event_type = publisher.topic_path(args.project,args.topic)
+    # print(event_type)
 
     #check if topic exists in pubsub, else create new one (service account my require additional role to create topics)
-    try:
-        publisher.get_topic(event_type)
-        logging.info('Using pub/sub topic {}'.format(args.topic))
-    except:
-        publisher.create_topic(event_type)
-        logging.info('Creating pub/sub topic {}'.format(topic))
+    # try:
+    #     publisher.get_topic(event_type)
+    #     logging.info('Using pub/sub topic {}'.format(args.topic))
+    # except:
+    #     #publisher.create_topic(event_type)
+    #     logging.info('Pub/sub topic {} does not exist'.format(args.topic))
 
 
 
